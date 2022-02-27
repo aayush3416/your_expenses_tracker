6599d28ffa78b3a06437b308e0fc0c953689dbec
Aayush Soni <87783633+aayush3416@users.noreply.github.com>
2022-02-26 22:07:12 -0500

    added server

---
+++ server/build/precache-manifest.bd32d7510b6ae48b512ba772e4c935a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7060bf550cc21944c82cdfe636827c2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1066973b1b12fb166f80",
+    "url": "/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1066973b1b12fb166f80",
+    "url": "/static/js/main.9edbf093.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
